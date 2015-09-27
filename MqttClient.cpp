ba44cb8be65067732f6de6a1ea1bccc0b1db717c
Antonio Grieco <antonio.grieco@hs-augsburg.de>
2015-09-27 10:32:49 +0200

    Added ability to set clientId and made "cleanSession" field affecting connection and meeting qmqtt's datatype.

--- MqttClient.cpp
+++ MqttClient.cpp
@@ -4,15 +4,19 @@
 MqttClient::MqttClient(QObject *parent) :
     QObject(parent)
 {
-	m_port = 1883;
-	m_qos = 0;
+    m_port = 1883;
+    m_qos = 0;
+    m_clientId = QString(QUuid::createUuid().toString());
+    m_cleanSession = true;
 //	m_keepalive = 60;
     // re-initialize connection whenever the
-	// host or the port changes
-	QObject::connect(this, SIGNAL(hostChanged(QString)),
-			this, SLOT(initializeConnection()));
-	QObject::connect(this, SIGNAL(portChanged(int)),
-			this, SLOT(initializeConnection()));
+    // host, port or the clientId changes
+    QObject::connect(this, SIGNAL(hostChanged(QString)),
+            this, SLOT(initializeConnection()));
+    QObject::connect(this, SIGNAL(portChanged(int)),
+            this, SLOT(initializeConnection()));
+    QObject::connect(this, SIGNAL(clientIdChanged(QString)),
+            this, SLOT(initializeConnection()));
 }
 
 
@@ -21,13 +25,15 @@ void MqttClient::initializeConnection()
     DEBUG;
     if(m_host.isEmpty() || !m_port || m_topic.isEmpty())
     {
-		DEBUG << "No host, port and topic is defined, initial suspended";
+        DEBUG << "No host, port and topic is defined, initial suspended";
 //        disconnect();
         return;
     }
 
     this->client = new QMQTT::Client(m_host, m_port);
-	this->client->setClientId(QString(QUuid::createUuid().toString()));
+
+    this->client->setClientId(m_clientId);
+    this->client->setCleansess(m_cleanSession);
 
 //    this->client->setUsername("user");
 //    this->client->setPassword("password");
@@ -38,11 +44,11 @@ void MqttClient::initializeConnection()
                      this, SLOT(subscribeToTopic(quint8)));
     QObject::connect(this->client, SIGNAL(subscribed(QString)),
                      this, SLOT(subscribedToTopic(QString)));
-	QObject::connect(this->client, SIGNAL(connected()),
-					 this, SIGNAL(connected()));
+    QObject::connect(this->client, SIGNAL(connected()),
+                     this, SIGNAL(connected()));
     QObject::connect(this->client, SIGNAL(disconnected()),
                      this, SIGNAL(disconnected()));
 
-	this->client->connect();
+    this->client->connect();
 }
 
